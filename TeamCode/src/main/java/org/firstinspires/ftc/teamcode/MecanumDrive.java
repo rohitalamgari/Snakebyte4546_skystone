fda1d653db3581fc590ce46e1de1fa21917e1b0a
Arya Anantula <anantula.arya@gmail.com>
2019-09-09 16:20:11 -0500

    Restructure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -1,5 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -14,9 +26,11 @@ public class MecanumDrive extends robotPart {
     public DcMotor mtrBL = null;
     public DcMotor mtrBR = null;
 
-    //method for declaring our sensors
-    Sensors sensors = new Sensors();
-
+    //gyro
+    public BNO055IMU gyro;
+    Orientation angles;
+    Acceleration gravity;
+    BNO055IMU.Parameters parameters;
 
     //these are the variables for
     int encodersPerRev = 1120;
@@ -43,8 +57,16 @@ public class MecanumDrive extends robotPart {
         mtrBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         mtrBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        //initializing our sensors
-        sensors.init(hwmap, myTelemetry);
+        //gyro parameters
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        gyro = hwmap.get(BNO055IMU.class, "imu");
+        gyro.initialize(parameters);
     }
 
     //this method sends power so that the robot will move in forwards/backwards vectors
@@ -70,6 +92,60 @@ public class MecanumDrive extends robotPart {
         mtrBR.setPower(-power);
         mtrFR.setPower(power);
         mtrBL.setPower(power);
+    }
+
+    public void updateValues(){
+        angles = gyro.getAngularOrientation();
+    }
+
+    public boolean resetGyro(){
+        return gyro.initialize(parameters);
+    }
+
+    //returns positive angle to turn right & negative angle to turn left
+    public double angleDiff(double goalAngle) {
+        double currAngle = gyroYaw();
+        if (currAngle >= 0 && goalAngle >= 0 || currAngle <= 0 && goalAngle <= 0) { //curr & goal are both positive or both negative
+            return -(currAngle - goalAngle);
+        } else if (Math.abs(currAngle - goalAngle) <= 180) {//diff btwn curr & goal is less than or equal to 180
+            return -(currAngle - goalAngle);
+        } else if (currAngle > goalAngle) {//curr is greater than goal
+            return (360 - (currAngle - goalAngle));
+        } else {//goal is greater than curr
+            return -(360 + (currAngle - goalAngle));
+        }
+    }
+
+    //gyro method to get yaw
+    public double gyroYaw(){
+        updateValues();
+        double yaw = angles.firstAngle * -1;
+        if (angles.firstAngle < -180){
+            yaw -= 360;
+        }
+        return yaw;
+    }
+
+    //gyro method to get pitch
+    public double gyroPitch(){
+        updateValues();
+        double pitch = angles.secondAngle;
+        return pitch;
+    }
+
+    //gyro method to get roll
+    public double gyroRoll(){
+        updateValues();
+        double roll = angles.thirdAngle;
+        return roll;
+    }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotSkystone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotSkystone.java
@@ -7,11 +7,9 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class RobotSkystone {
 
     MecanumDrive driveTrain = new MecanumDrive();
-    Sensors sensors = new Sensors();
 
     public void init(HardwareMap hwmap, Telemetry telemetry){
         //this is where we initialize all of our other things, like sensors, drivetrain, etc.
         driveTrain.init(hwmap, telemetry);
-        sensors.init(hwmap, telemetry);
     }
 }
