39401d336281f69de4a9366cf4a7a18b4b3d965c
Arya Anantula <anantula.arya@gmail.com>
2019-09-16 17:35:59 -0500

    Setup for Vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaBitmap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaBitmap.java
@@ -17,19 +17,87 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.Parameters;
 
+import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+
 public class VuforiaBitmap {
 
     private LinearOpMode opMode;
 
-    VuforiaLocalizer vuforia;
-    private VuforiaLocalizer.Parameters parameters;
-    private VuforiaLocalizer.CameraDirection CAMERA_CHOICE = VuforiaLocalizer.CameraDirection.BACK;
+    private VuforiaLocalizer vuforia;
+    private Parameters parameters;
+    private CameraDirection CAMERA_CHOICE = CameraDirection.BACK;
     private static final String VUFORIA_KEY = "Acwi41P/////AAABmXAF5Uahj0aglVwEx0GLTotkFwuYvGa385NRnC3GmFdHiha7BKdStHJwB6nj4zrSBLOJ0jGEICqTReR3LiErc63MaNJf8NR/J8TUk6MOaF8xM5fa5uDU3J/7/tys+Hu1G5nlncWy3gGsHrU8lwG/rL+G0R/caVfNp0GfRtpcH7LMLDZOslSc+URv9+IF8+C0jA4JzTfM4lRkOEcIqIyTs20EZC+W3QYI7o7n700hOwq+WpoG7qMgqcrgk3+B1/hTLICE3fodM/34CQjbEONYKpGbj8IOG714CeY9qyI6WhainXidKda/QAslXEvYCDvBCZoGW/4I3TaZAJUWAeD1l5SeL/m4nuJxV9Jmai/0/9Qn";
 
+    private BlockingQueue<VuforiaLocalizer.CloseableFrame> frame;
+
+    public static String skystonePosition;
 
     private final int RED_THRESHOLD = 0;
     private final int GREEN_THRESHOLD = 0;
     private final int BLUE_THRESHOLD = 0;
+
+    public VuforiaBitmap(LinearOpMode opMode){
+
+        this.opMode = opMode;
+
+        int cameraMonitorViewId = this.opMode.hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", this.opMode.hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        params.vuforiaLicenseKey = VUFORIA_KEY;
+        params.cameraDirection = CAMERA_CHOICE;
+        vuforia = ClassFactory.getInstance().createVuforia(params);
+
+        Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true);
+        vuforia.setFrameQueueCapacity(4);
+        vuforia.enableConvertFrameToBitmap();
+
+    }
+
+    public Bitmap getBitmap() throws InterruptedException{
+
+        VuforiaLocalizer.CloseableFrame picture;
+        picture = vuforia.getFrameQueue().take();
+        Image rgb = picture.getImage(1);
+
+        long numImages = picture.getNumImages();
+
+        opMode.telemetry.addData("Num images", numImages);
+        opMode.telemetry.update();
+
+        for (int i = 0; i < numImages; i++) {
+
+            int format = picture.getImage(i).getFormat();
+            if (format == PIXEL_FORMAT.RGB565) {
+                rgb = picture.getImage(i);
+                break;
+            } else {
+                opMode.telemetry.addLine("Didn't find correct RGB format");
+                opMode.telemetry.update();
+
+
+            }
+        }
+
+        Bitmap imageBitmap = Bitmap.createBitmap(rgb.getWidth(), rgb.getHeight(), Bitmap.Config.RGB_565);
+        imageBitmap.copyPixelsFromBuffer(rgb.getPixels());
+
+        opMode.telemetry.addData("Image width", imageBitmap.getWidth());
+        opMode.telemetry.addData("Image height", imageBitmap.getHeight());
+        opMode.telemetry.update();
+
+
+        picture.close();
+
+        opMode.telemetry.addLine("Got bitmap");
+        opMode.telemetry.update();
+
+        return imageBitmap;
+    }
+
+
+
+
 
 
 
