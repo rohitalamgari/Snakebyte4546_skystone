f38c8cc503cd3bbad0d444ad5e262223d0e8e989
Arya Anantula <anantula.arya@gmail.com>
2019-09-16 16:49:25 -0500

    Revert "Revert "upgrade to big dick energy""
    This reverts commit cf4ff8bcd7921608081dd30d6a26e3b0cf373bb6.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotPart.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotPart.java
@@ -4,9 +4,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
 public abstract class robotPart {
     protected Telemetry privateTelemetry;
+
 
     public void init(HardwareMap hwmap, Telemetry myTelemetry){
         privateTelemetry = myTelemetry;
